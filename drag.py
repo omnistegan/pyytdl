0d08c199ce1d47100cc03dfccbf0b61648b11fd4
Graham Bouvier <gmbouvier@gmail.com>
2015-11-10 17:44:56 -0800

    Initial Commit

---
+++ drag.py
@@ -0,0 +1,70 @@
+#!/bin/python
+
+from gi.repository import Gtk, Gdk, GObject
+from youtube import *
+
+
+class MainWindow(Gtk.Window):
+
+
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Drag and Drop")
+
+        self.set_default_size(250, 250)
+
+        box = Gtk.Box()
+        self.add(box)
+
+        self.drop_area = DropArea()
+
+        box.pack_start(self.drop_area, True, True, 0)
+
+        self.drop_area.drag_dest_set_target_list(None)
+        self.drop_area.drag_dest_add_text_targets()
+
+        self.progressbar = Gtk.ProgressBar()
+        box.pack_start(self.progressbar, True, True, 0)
+
+        self.timeout_id = GObject.timeout_add(50, self.on_timeout, None)
+
+
+    def on_timeout(self, user_data):
+        """
+        Update value on the progress bar
+        """
+        try:
+            self.progressbar.set_fraction(self.video.percent_downloaded)
+            print(self.video.percent_downloaded)
+        except AttributeError:
+            pass
+
+        # As this is a timeout function, return True so that it
+        # continues to get called
+        return True
+
+
+    def create_video_instance(self, url):
+        self.video = Video(url)
+        self.video.download_and_play_av()
+
+
+
+class DropArea(Gtk.Label):
+
+    def __init__(self):
+        Gtk.Label.__init__(self)
+        self.drag_dest_set(Gtk.DestDefaults.ALL, [], Gdk.DragAction.COPY)
+
+        self.connect("drag-data-received", self.on_drag_data_received)
+
+
+    def on_drag_data_received(self, widget, drag_context, x, y, data, info, time):
+        win.create_video_instance(data.get_text())
+
+
+
+if __name__ == '__main__':
+    win = MainWindow()
+    win.connect("delete-event", Gtk.main_quit)
+    win.show_all()
+    Gtk.main()
---
+++ youtube.py
@@ -0,0 +1,135 @@
+#!/bin/python
+
+import youtube_dl
+
+import json
+import os
+import subprocess
+from time import sleep
+from multiprocessing import Process
+from glob import glob
+
+
+DOWNLOAD_FOLDER = os.path.expanduser('~/Downloads/Youtube/')
+
+
+
+def start_thread(function, *args):
+    process = Process(target=function, args=args)
+    process.start()
+    return process
+
+
+class Video():
+
+    def return_json(self, dump):
+        return json.dumps(dump)
+
+    def return_formats(self):
+        return self.results['formats']
+
+    def find_best_audio(self, max_abr=None):
+        format_abr = []
+        for each in self.formats:
+            if (
+                'abr' in each.keys() and
+                (max_abr == None or each['abr'] <= max_abr)
+            ):
+                format_abr.append((each['format_id'], each['abr']))
+        format_abr.sort(key=lambda tup: tup[1], reverse=True)
+        return format_abr[0][0]
+
+    def find_best_video(self, max_height=None):
+        format_filesize = []
+        for each in self.formats:
+            if (
+                ('height' in each.keys() and each['filesize'] != None) and
+                (max_height == None or
+                    (each['height'] != None and each['height'] <= max_height)
+                )
+            ):
+                format_filesize.append((each['format_id'], each['height'], each['filesize']))
+        format_filesize.sort(key=lambda tup: tup[2], reverse=True)
+        return format_filesize[0][0]
+
+    def find_best_av(self, max_height=None):
+        format_filesize = []
+        for each in self.formats:
+            if (
+                'DASH' not in each['format_note'] and
+                'height' in each.keys() and
+                ( max_height == None or each['height'] <= max_height )
+            ):
+                format_filesize.append((each['format_id'], each['height'] ))
+        format_filesize.sort(key=lambda tup: tup[1], reverse=True)
+        return format_filesize[0][0]
+
+    def download(self, avformat):
+        options = {
+            'noplaylist': True,
+            'format': avformat,
+            'outtmpl': DOWNLOAD_FOLDER + '%(uploader)s - %(title)s - %(id)s - %(format)s.%(ext)s',
+            'nopart': True,
+            'progress_hooks': [self.status_hook],
+        }
+        with youtube_dl.YoutubeDL(options) as ydl:
+            results = ydl.extract_info(self.url, download = True)
+
+    def status_hook(self, ytdl_ob):
+        if ytdl_ob['status'] == 'finished':
+            self.percent_downloaded = 1.0
+            print('Youtube Download: Finished downloading')
+        if ytdl_ob['status'] == 'downloading':
+            self.percent_downloaded = ytdl_ob['downloaded_bytes']/ytdl_ob['total_bytes']
+            print('\n' + str(self.percent_downloaded))
+
+    def play_with_mpv(self, timeout=10):
+        # Wait for the file to appear
+        count = timeout
+        while count >= 0:
+            if [n for n in glob(DOWNLOAD_FOLDER + '*' + self.id + '*') if os.path.isfile(n)] != []:
+                break
+            else:
+                sleep(1)
+                count -= 1
+            if count == 0:
+                print('Youtube Download: Download Timeout')
+                exit()
+        # Use glob to select the full correct filename to use
+        video_file_path = [n for n in glob(DOWNLOAD_FOLDER + '*' + self.id + '*') if os.path.isfile(n)][0]
+        # Wait for the file to be large enough to play (~4MB)
+        count = timeout
+        while count >= 0:
+            if os.path.getsize(video_file_path) > 4000000:
+                break
+            else:
+                sleep(1)
+                count -= 1
+            if count == 0:
+                print('Youtube Download: File did not reach 4MB, attempting to play')
+                break
+        # Play the file with mpv, --keep-open is used to not close the file if it reaches EOF due to slow download
+        subprocess.call(['mpv', video_file_path, '--really-quiet' , '--keep-open'])
+        self.download_process.terminate()
+
+
+    def download_and_play_av(self):
+        self.download_process = start_thread(self.download, self.find_best_av())
+        self.mpv_process = start_thread(self.play_with_mpv, )
+
+
+    def download_and_mux_best_av(self, max_height=1080):
+        try:
+            self.download(self.find_best_video(max_height) + '+' + self.find_best_audio())
+            self.play_with_mpv()
+        except:
+            self.download_and_play_av()
+
+    def __init__(self, url):
+        with youtube_dl.YoutubeDL() as ydl:
+            self.results = ydl.extract_info(url, download = False)
+        self.url = url
+        self.formats = self.return_formats()
+        self.id = self.results['id']
+        self.video_file_path = ''
+        self.percent_downloaded = 0.1
